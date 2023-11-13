41e2cefcbe5290f75017574903a439cb0b3c31c8
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-11-13 16:40:35 +0530

    min number of jumpes

---
+++ algoexpert/Dynamic Programming/minNumOfJumps.py
@@ -0,0 +1,10 @@
+def minNumberOfJumps(array):
+    # Write your code here.
+    jumps = [float("inf") for x in array]
+    jumps[0] = 0
+    for i in range(1, len(array)):
+        for j in range(0,i):
+            if array[j] +j >= i:
+                jumps[i] = min(jumps[j]+1, jumps[i])
+                
+    return jumps[-1]
