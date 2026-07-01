a335eb2a3bac1f64ed6f164276ee06b6fd97fd12
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-07-01 08:05:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subarray with 0 sum/subarray-with-0-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} arr
+ * @returns {boolean}
+ */
+class Solution {
+    // Function to check whether there is a subarray present with 0-sum or not.
+    subArrayExists(arr) {
+        // code here
+        let prefixSum = 0;
+        let map = {};
+        for(let num of arr) {
+            prefixSum +=num;
+            if(prefixSum ===0 || map[prefixSum]){
+                return true;
+            }
+            map[prefixSum] = true;
+        }
+    return false;
+    }
+}
